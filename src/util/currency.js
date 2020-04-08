3eec571158b9b691c4a83ba2a692a9dd3efcf4dd
matthunterking <matthew85king@gmail.com>
2020-04-08 11:57:45 +0100

    Fixed currency formater

--- src/util/currency.js
+++ src/util/currency.js
@@ -1,5 +1,9 @@
 export const formatToCurrency = (number) => {
  if (number < 1) return `${number * 100}p`;
- if (number > 999) return `£${number / 1000},000`;
+ if (number > 999) {
+  const thousands = Math.floor(number / 1000);
+  const hundreds = Math.floor((number % 1000) / 100);
+  return `£${thousands},${hundreds}00`
+ };
  return `£${number}`
 }
