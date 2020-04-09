2f44f49c698eaa0ff589b272cf1fff762b27a549
matthunterking <matthew85king@gmail.com>
2020-04-09 11:20:47 +0100

    Added styling to deal modal

--- src/components/DealModal.js
+++ src/components/DealModal.js
@@ -1,6 +1,9 @@
 import React, { Fragment } from 'react';
-import { Modal, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { formatToCurrency } from '../util/currency'
+import { Modal, View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { formatToCurrency } from '../util/currency';
+import { DEAL_NO_DEAL_BUTTON } from '../constants/theme';
+
+const phoneImage = require('../assets/phone.png');
 
 const DealModal = ({ isVisable, currentOffer, takeDeal, noDeal, dealtAt, closeModal }) => {
  return <Modal
@@ -10,8 +13,9 @@ const DealModal = ({ isVisable, currentOffer, takeDeal, noDeal, dealtAt, closeMo
  >
   <View style={styles.outerContainer}>
    <View style={styles.container}>
-    <Text>The banker offers you</Text>
-    <Text>{formatToCurrency(currentOffer)}</Text>
+    <Image source={phoneImage} />
+    <Text style={styles.offerText}>The banker offers you</Text>
+    <Text style={styles.offer}>{formatToCurrency(currentOffer)}</Text>
     {dealtAt ?
      <Fragment>
       <Text>You dealt at {formatToCurrency(dealtAt)}</Text>
@@ -22,10 +26,10 @@ const DealModal = ({ isVisable, currentOffer, takeDeal, noDeal, dealtAt, closeMo
      :
      <Fragment>
       <TouchableOpacity style={styles.button} onPress={takeDeal}>
-       <Text>Deal</Text>
+       <Text style={styles.dealNoDealText}>Deal</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.button} onPress={noDeal}>
-       <Text>No Deal</Text>
+       <Text style={styles.dealNoDealText}>No Deal</Text>
       </TouchableOpacity>
      </Fragment>
     }
@@ -41,17 +45,32 @@ const styles = StyleSheet.create({
   alignItems: 'center'
  },
  container: {
-  backgroundColor: 'pink',
-  height: 300,
-  width: 300,
+  backgroundColor: 'white',
+  borderRadius: 10,
+  borderWidth: 3,
+  height: 500,
+  width: 350,
   justifyContent: 'center',
   alignItems: 'center'
  },
  button: {
-  backgroundColor: 'red',
-  padding: 30,
-  width: 150,
-  margin: 5
+  backgroundColor: DEAL_NO_DEAL_BUTTON,
+  borderRadius: 40,
+  padding: 15,
+  width: 250,
+  margin: 10
+ },
+ dealNoDealText: {
+  fontSize: 25,
+  textAlign: 'center',
+  textTransform: 'uppercase'
+ },
+ offerText: {
+  fontSize: 25
+ },
+ offer: {
+  fontSize: 30,
+  fontWeight: 'bold'
  }
 })
 
--- src/constants/theme.js
+++ src/constants/theme.js
@@ -2,3 +2,4 @@ export const BOX_RED = 'rgb(219, 0, 0)';
 export const SEAL_RED = 'rgb(239, 0, 0)';
 export const AMOUNT_RED = 'rgb(177, 0, 0)';
 export const AMOUNT_BLUE = 'rgb(21, 24, 109)';
+export const DEAL_NO_DEAL_BUTTON = 'rgb(10, 142, 244)';
