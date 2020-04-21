f243b0b3ad29b67035a20fb02de05cd10947b0c0
matthunterking <matthew85king@gmail.com>
2020-04-21 11:20:37 +0100

    Reverted compress

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,31 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { View, StyleSheet, FlatList } from 'react-native';
 import { deviceWidth } from '../constants/device';
 import { values } from '../constants/game';
 import Amount from './Amount';
 
 
-const GameBoard = ({ boxValues, compressValues = true }) => {
+const GameBoard = ({ boxValues }) => {
  const allValues = values.sort((a, b) => a - b);
  const openValues = boxValues.filter(box => box.isOpened).map(({ value }) => value);
- const remainingValues = compressValues && allValues.filter(value => !openValues.includes(value)).reduce((acc, value) => {
-  const isHigh = value > 999
-  isHigh ? acc.high.push(value) : acc.low.push(value);
-  isHigh ? acc.high.sort((a, b) => a - b) : acc.low.sort((a, b) => a - b);
-  return acc;
- }, { low: [], high: [] });
 
  return (
   <View style={styles.container}>
    <FlatList
     style={styles.list}
-    data={remainingValues ? remainingValues.low : allValues.slice(0, 11)}
+    data={allValues.slice(0, 11)}
     keyExtractor={item => `value-${item}`}
     renderItem={({ item }) => <Amount value={item} isOpened={openValues.includes(item)} isLarge />}
    />
    <FlatList
     style={styles.list}
-    data={remainingValues ? remainingValues.high : allValues.slice(11, 22)}
+    data={allValues.slice(11, 22)}
     keyExtractor={item => `value-${item}`}
     renderItem={({ item }) => <Amount value={item} isOpened={openValues.includes(item)} isLarge />}
    />
